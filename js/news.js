d9396239500c075710f93dcf82c52afcfe1b9788
max <90maxgreenwood@gmail.com>
2014-09-23 19:11:44 +0000

    functioning news-feed

--- js/news.js
+++ js/news.js
@@ -1,25 +1,47 @@
-var main = function(){
- var footballList = new ajaxLinks("http://content.guardianapis.com/search?api-key=test&show-fields=football&show-elements=football&q=football",'#news','#news1','#news2','#news3','#news4');
+$(document).ready(function() {
+ 
+ 
+var fillNews = function(sect) {
 
- var UKnewsList = new ajaxLinks("http://content.guardianapis.com/search?api-key=test&show-fields=football&show-elements=uk%20news&order-by=newest&q=uk%20news",'#news5','#news6','#news7','#news8','#news9');
+$.ajax({
+   type: "GET",
+   dataType: "jsonp",
+   cache: false,
+   url: "http://content.guardianapis.com/search?api-key=test&show-fields=all&order-by=newest&section=" + sect,
+   success: function(data) {
+       //window.alert("in ajax" + sect);
+       for (var i = 0; i < 5; i++) {
+         if  (i === 0) {
+       $("#" + sect).append("<img src='" + data.response.results[i].fields.thumbnail + "'>");
+         }
+         
+       $("#" + sect).append((i+1) + ". " + "<a href='"
+           + data.response.results[i].webUrl + "'>"
+           + data.response.results[i].webTitle + "</a><br>"
+              + data.response.results[i].fields.trailText + "<br><br>");
+           }
+       }
+   });
 
- var TravelList = new ajaxLinks("http://content.guardianapis.com/search?api-key=test&show-fields=travel&show-elements=travel&order-by=newest&q=travel",'#news10','#news11','#news12','#news13','#news14');
-}
-$(document).ready(main);
-
-
-var ajaxLinks = function(link,div0,div1,div2,div3,div4){
- $.ajax({
-   url: link,
-   success: function( data ) {
-     $(div0).html(data.response.results[1].webTitle);
-     $(div1).html(data.response.results[2].webTitle);
-     $(div2).html(data.response.results[3].webTitle);
-     $(div3).html(data.response.results[4].webTitle);
-     $(div4).html(data.response.results[5].webTitle);
-   }
-});
 };
-$(function() {
-    $( "#tabs" ).tabs();
-  });
+
+fillNews ("uk-news");
+fillNews ("football");
+fillNews ("travel");
+
+ $("div#tabs").tabs();
+
+   $("#plus").click(function() {
+
+       var num_tabs = $("div#tabs ul li").length;
+
+      $("div#tabs ul").append(
+           "<li><a href='#tab" + num_tabs + "'>#" + num_tabs + "</a></li>"
+       );
+
+       $("div#tabs").tabs("refresh");
+   });
+ });
+
+
+ 
