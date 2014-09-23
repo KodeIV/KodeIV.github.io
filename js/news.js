ae446dd7d0e47bd98369694ba8dc3601005bb076
max <90maxgreenwood@gmail.com>
2014-09-23 21:35:55 +0000

    news; css,js,html edit

--- js/news.js
+++ js/news.js
@@ -1,32 +1,33 @@
 $(document).ready(function() {
  
- 
-var fillNews = function(sect) {
+
+ var fillNews = function (sect) {                                                                              // function created, names 'fillNews'. Passed 'sect' as a parameter.
 
 $.ajax({
    type: "GET",
-   dataType: "jsonp",
+   dataType: "jsonp",                                                                                           // necessary AJAX name/values
    cache: false,
-   url: "http://content.guardianapis.com/search?api-key=test&show-fields=all&order-by=newest&section=" + sect,
+   url: "http://content.guardianapis.com/search?api-key=test&show-fields=all&order-by=newest&section=" + sect,  // the 'sect' parameter is added on to the api link, this specifies a topic
    success: function(data) {
        //window.alert("in ajax" + sect);
-       for (var i = 0; i < 5; i++) {
-         if  (i === 0) {
-       $("#" + sect).append("<img src='" + data.response.results[i].fields.thumbnail + "'>");
+       for (var i = 0; i < 5; i++) {                                                                            // creates a 'for' loop that cycles through the api data, stops at the 5th.
+         if  (i === 0) {                                                                                        // when 'i' is 0,
+       $("#" + sect).append("<img src='" + data.response.results[i].fields.thumbnail + "'>");                   // a thumbnail is appended to the 'sect'id.
          }
-         
-       $("#" + sect).append((i+1) + ". " + "<a href='"
-           + data.response.results[i].webUrl + "'>"
-           + data.response.results[i].webTitle + "</a><br>"
-              + data.response.results[i].fields.trailText + "<br><br>");
+                                                                                             // else
+       $("#" + sect).append((i+1) + ". " + "<a href='"                                                          //  THIS IS ONE APPEND. 
+           + data.response.results[i].webUrl + "'>"                                                             //
+           + data.response.results[i].webTitle + "</a><br>"                                                     //
+              + data.response.results[i].fields.trailText + "<br><br>");                                        // trail text adds the 'trailing text'
+                                                             
            }
        }
    });
 
 };
 
-fillNews ("uk-news");
-fillNews ("football");
+fillNews ("uk-news");                                                        
+fillNews ("football");                                                                                          //
 fillNews ("travel");
 
  $("div#tabs").tabs();
