3f502355c69b6baa055c7b2d1ecfc11af43f541f
Naomi Gaynor <naomi.gaynor@gmail.com>
2014-10-09 12:45:59 +0100

    specified that array is a json file

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var server = http.createServer(function (request, response){
 	twitterimages(function (err, res){
 		response.write(res);
 
-		fs.readFile(array, function(err, data){
+		fs.readFile(array.json, function(err, data){
 			response.writeHead(200, {"Content-Type": "text/json", "Access-Control-Allow-Origin":"*"});
 			response.end(data);
 		})
