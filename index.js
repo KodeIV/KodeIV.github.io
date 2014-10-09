e6a48e7bc2ea16dfc6521eb7d0f83080ae1b37d2
Naomi Gaynor <naomi.gaynor@gmail.com>
2014-10-09 12:11:29 +0100

    including array.json in index.js

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var fs = require('fs');
 var url = require('url');
 
 var server = http.createServer(function (request, response){
-	twitterimages(function (err, res){
+	response.setHeader("Content-Type", "text/plain");
+	request.twitterimages.array.json(function (err, res){
 		response.write(res);
 	})
 })
